c9674520f72e5743ba19c0ee6d11991153a2c9dc
Noah Lee <noahjefferson.lee@calbaptist.edu>
2023-11-02 18:12:44 -0700

    Did testing for walkup ticket

--- src/TicketClient.java
+++ src/TicketClient.java
@@ -6,9 +6,23 @@ public class TicketClient {
         Ticket noahTicket = new Ticket(20);
         Ticket jesusTicket = new Ticket(30.4);
 
+        System.out.println("===Normal Tickets===");
         System.out.println(kipTicket);
         System.out.println(noahTicket);
         System.out.println(jesusTicket);
+        System.out.println("===Normal Tickets===");
+
+
+
+        WalkupTicket kipWalkup = new WalkupTicket(40.0, 42069);
+        WalkupTicket noahWalkup = new WalkupTicket(20);
+        WalkupTicket jesusWalkup = new WalkupTicket(30.4);
+
+        System.out.println("===Walkup Tickets===");
+        System.out.println(kipWalkup);
+        System.out.println(noahWalkup);
+        System.out.println(jesusWalkup);
+        System.out.println("===Walkup Tickets===");
 
     }
 }
