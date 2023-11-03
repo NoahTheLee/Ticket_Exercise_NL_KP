e2a4a52a346cb663a8a279d36417fb6d43140c76
Noah Lee <noahjefferson.lee@calbaptist.edu>
2023-11-02 18:05:08 -0700

    Did the first part of testing for basic tickets

---
+++ src/TicketClient.java
@@ -0,0 +1,14 @@
+public class TicketClient {
+
+    public static void main(String[] args){
+
+        Ticket kipTicket = new Ticket(40.0, 42069);
+        Ticket noahTicket = new Ticket(20);
+        Ticket jesusTicket = new Ticket(30.4);
+
+        System.out.println(kipTicket);
+        System.out.println(noahTicket);
+        System.out.println(jesusTicket);
+
+    }
+}
